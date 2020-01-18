a291fdfb3bbe78529fbb8cfe799f0896cc80c33e
dev <developer@byes-pa.ch>
2020-01-18 14:43:15 +0100

    Added init-vscode argument (for debugging)

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ namespace CSharpScriptRunner
             public const string Install = "install";
             public const string New = "new";
             public const string ClearCache = "clear-cache";
+            public const string InitVSCode = "init-vscode";
         }
 
         [STAThread]
@@ -57,7 +58,8 @@ namespace CSharpScriptRunner
                 {
                     case Verbs.Install: Install(); break;
                     case Verbs.New: CreateNew(args.Length > 1 ? args[1] : null); break;
-                    case Verbs.ClearCache: ClearCache(); break; 
+                    case Verbs.ClearCache: ClearCache(); break;
+                    case Verbs.InitVSCode: InitVSCode(); break;
                     default: RunScript(args); break;
                 }
             }
@@ -251,6 +253,13 @@ namespace CSharpScriptRunner
 
         static void RunScript(string[] args)
         {
+            if (!File.Exists(args[0]))
+            {
+                WriteLine($"Script file '{args[0]}' does not exist.", ConsoleColor.Red);
+                System.Threading.Thread.Sleep(5000);
+                return;
+            }
+
             var scriptPath = Path.GetFullPath(args[0]);
             var runtimeExt = Path.GetExtension(Path.GetFileNameWithoutExtension(scriptPath));
             if (!string.IsNullOrEmpty(runtimeExt))
@@ -388,6 +397,40 @@ namespace CSharpScriptRunner
                 catch (UnauthorizedAccessException) { }
             }))).Wait();
             WriteLine("Cache cleared", ConsoleColor.Green);
+        }
+
+        static void InitVSCode()
+        {
+            const string VSCodeDir = ".vscode";
+            const string Filter = ".Templates.vscode.";
+
+            var templates = typeof(Program).Assembly.GetManifestResourceNames()
+                .Select(x => (i: x.IndexOf(Filter), s: x))
+                .Where(x => x.i > -1)
+                .Select(x => (name: x.s, File: Path.Combine(VSCodeDir, x.s.Substring(x.i + Filter.Length))));
+
+            var dir = new DirectoryInfo(VSCodeDir);
+            if (!dir.Exists)
+            {
+                dir.Create();
+                // dir.Attributes |= FileAttributes.Hidden;
+            }
+
+            foreach (var template in templates)
+            {
+                if (File.Exists(template.File))
+                {
+                    WriteLine($"File '{template.File}' already exists.", ConsoleColor.Yellow);
+                    continue;
+                }
+
+                using (var stream = typeof(Program).Assembly.GetManifestResourceStream(template.name))
+                using (var file = File.OpenWrite(template.File))
+                {
+                    WriteLine($"Creating file '{template.File}'...", ConsoleColor.Green);
+                    stream.CopyTo(file);
+                }
+            }
         }
 
         static void WriteLine(string line, ConsoleColor color = (ConsoleColor)(-1))
